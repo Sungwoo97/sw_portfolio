e9c97681eb1ed7c79b24b6223b1afd0bb377824a
Sungwoo97 <gwaja97@gmail.com>
2025-01-13 17:44:42 +0900

    React로 이동 (타이핑 효과)

---
+++ src/sections/hero/TypingHero.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import { TypeAnimation } from "react-type-animation";
+
+const TypingHero = () => {
+  return (
+    <div class="text-container">
+      <TypeAnimation
+        sequence={[
+          "FRONT-END", // Types 'One'
+          4000, // Waits 1s
+          "", // Types 'One'
+          2000, // Waits 1s
+          "SOFT WEB", // Deletes 'One' and types 'Two'
+          4000, // Waits 2s
+          "", // Types 'One'
+          2000, // Waits 1s
+          () => {
+            console.log("Sequence completed");
+          },
+        ]}
+        speed={5}
+        deletionSpeed={5}
+        wrapper="span"
+        cursor={false}
+        repeat={Infinity}
+        style={{ fontSize: "100px", display: "inline-block", opacity: "20%" }}
+      />
+      <p className="typeText"></p>
+    </div>
+  );
+};
+
+export default TypingHero;
