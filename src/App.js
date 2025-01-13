eedfa8ddd68e32e6dda7132fb6d499b0385c7d1e
Sungwoo97 <gwaja97@gmail.com>
2025-01-13 13:43:10 +0900

    React로 이동 (test)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import "./App.css";
+import React from "react";
+
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
