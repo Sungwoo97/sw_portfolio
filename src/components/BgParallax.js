91cd9cb3ade379308095a4db6fb03c09e4fa9501
Sungwoo97 <gwaja97@gmail.com>
2025-01-15 09:59:32 +0900

    React로 이동(Bg)
    bg rellax 컴포넌트로 생성하여 재사용하기 편리하게 생성

---
+++ src/components/BgParallax.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { ParallaxProvider, Parallax, useParallax } from "react-scroll-parallax";
+
+const BgParallax = () => {
+  return (
+    <ParallaxProvider>
+      <section className="bg">
+        <Parallax className="middle_bg" easing={"ease"} speed={-5}>
+          <img src="./img/dev_env.png" alt="bg1" />
+        </Parallax>
+      </section>
+    </ParallaxProvider>
+  );
+};
+export default BgParallax;
