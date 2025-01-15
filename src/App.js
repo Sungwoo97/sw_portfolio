8d209efa2c30f3c546dc540b41c51f1fd5d96a3e
Sungwoo97 <gwaja97@gmail.com>
2025-01-15 20:43:06 +0900

    data.json 통합

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 
-import work from "./work.json";
-import list from "./list.json";
-import mini from "./mini.json";
+import data from "./data.json";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          element={<Home work={work} list={list} mini={mini} />}
-        />
+        <Route path="/" element={<Home data={data} />} />
       </Routes>
     </Router>
   );
