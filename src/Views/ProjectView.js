cd61b0fca468020001608f698d64cd20c0b43cc5
Sungwoo97 <gwaja97@gmail.com>
2025-01-16 23:57:47 +0900

    프로젝트 목록 스타일 변경

--- src/Views/ProjectView.js
+++ src/Views/ProjectView.js
@@ -4,9 +4,9 @@ import List from "../sections/project/List";
 const ProjectView = ({ list }) => {
   return (
     <section class="projectList container" id="project">
-      <div class="content row">
+      <div class="content ">
         <h4>저는 이런 프로젝트를 진행했어요</h4>
-        <div class="listWrapper col-md-12">
+        <div class="listWrapper row">
           {list.map((list) => (
             <List list={list} />
           ))}
--- src/sections/project/List.js
+++ src/sections/project/List.js
@@ -9,27 +9,21 @@ const List = ({ list }) => {
     AOS.init();
   }, []);
   return (
-    <div class="listSlides col-md-8 row" key={list.id} data-aos="fade-up">
-      <div class="thumbnail col-md-6">
+    <div class="listSlides col-md-4">
+      <div class="listContent">
         <img src={list.image} alt="" />
-        <a href={`/detail/${list.id}`} class="button btn hover_btn">
-          상세 보기
-        </a>
-      </div>
-      <div class="info col-md-6">
-        <h5>{list.title}</h5>
-        <p>{list.duration}</p>
-        <ul>
-          {list.details.map((detail, index) => (
-            <li key={index}> {detail}</li>
-          ))}
-        </ul>
-        <div class="gitLink">
-          <a href={list.links.github} target="_blank">
-            <img src="./img/github.svg" width="10" alt="github" />
+
+        <div class="listinfo">
+          <h5>{list.title}</h5>
+          <a href={`/detail/${list.id}`} class="button btn hover_btn">
+            상세 보기
           </a>
-          <a href={list.links.live} target="_blank">
-            <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+          <a
+            href={list.links.live}
+            target="_blank"
+            class="button btn hover_btn"
+          >
+            이동하기
           </a>
         </div>
       </div>
