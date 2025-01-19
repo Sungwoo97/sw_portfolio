7f759035c5f3c751befe304dfd9f5967dc5d27e0
Sungwoo97 <gwaja97@gmail.com>
2025-01-19 16:17:45 +0900

    SEO 관련 헬멧 추가

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -17,21 +17,22 @@ const Detail = ({ data }) => {
   const cssPath = `${process.env.PUBLIC_URL}/css/detail.css`;
 
   //페이지 별로 다른 css 파일을 적용하기 위해 useEffect 사용
-  useEffect(() => {
-    const link = document.createElement("link");
-    link.rel = "stylesheet";
-    link.href = cssPath;
-    document.head.appendChild(link);
+  // useEffect(() => {
+  //   const link = document.createElement("link");
+  //   link.rel = "stylesheet";
+  //   link.href = cssPath;
+  //   document.head.appendChild(link);
 
-    return () => {
-      document.head.removeChild(link); // 페이지 변경 시 제거
-    };
-  }, [cssPath]);
+  //   return () => {
+  //     document.head.removeChild(link); // 페이지 변경 시 제거
+  //   };
+  // }, [cssPath]);
 
   if (!item) return <p>Item not found</p>;
 
   return (
     <>
+      <link rel="stylesheet" href="./../css/detail.css" precedence="default" />
       <Header />
       <main>
         <div class="detail container">
