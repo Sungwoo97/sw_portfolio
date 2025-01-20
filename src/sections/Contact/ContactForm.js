6941d531dab91a826fb7c0c09b7c8ffb285effea
Sungwoo97 <gwaja97@gmail.com>
2025-01-21 05:11:58 +0900

    Contact EmailJs 구현

--- src/sections/Contact/ContactForm.js
+++ src/sections/Contact/ContactForm.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
+import emailjs from "emailjs-com";
 
-const AboutView = () => {
-  // 폼 제출 이벤트 핸들러
+function ContactForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
   const handleSubmit = (e) => {
-    e.preventDefault(); // 페이지 새로고침 방지
-    alert("폼이 제출되었습니다!"); // 추후 Supabase 연동으로 변경 가능
+    e.preventDefault();
+    emailjs
+      .send(
+        "service_4n175kl", // EmailJS에서 받은 Service ID
+        "template_08vd9p7", // EmailJS에서 받은 Template ID
+        formData,
+        "Ayg0-ehTLggPXavZK" // EmailJS에서 받은 Public Key
+      )
+      .then(
+        (result) => {
+          alert("Email sent successfully!");
+          setFormData({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          alert("Failed to send email: " + error.text);
+        }
+      );
   };
   return (
     <form action="/" className="col-md-6" onSubmit={handleSubmit}>
@@ -12,21 +42,29 @@ const AboutView = () => {
       <input
         type="text"
         id="name"
+        name="name"
         placeholder="Name"
+        value={formData.name}
+        onChange={handleChange}
         aria-label="Enter your name"
       />
       <label htmlFor="email" className="hidden"></label>
       <input
         type="email"
         id="email"
+        name="email"
         placeholder="Email"
+        value={formData.email}
+        onChange={handleChange}
         aria-label="Enter your email"
       />
-      <label htmlFor="Message" className="hidden"></label>
+      <label htmlFor="message" className="hidden"></label>
       <textarea
-        name="Message"
-        id="Message"
-        placeholder="Message"
+        name="message"
+        id="message"
+        placeholder="message"
+        value={formData.message}
+        onChange={handleChange}
         aria-label="Enter your message"
       ></textarea>
       <button type="submit" className="btn">
@@ -34,5 +72,5 @@ const AboutView = () => {
       </button>
     </form>
   );
-};
-export default AboutView;
+}
+export default ContactForm;
